f7205fcb754edb91cd872d3bf85450a485ab02a7
Tea <icepie.dev@gmail.com>
2022-04-30 01:51:11 +0800

    pc: use iframe

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,35 @@ export default Vue.extend({
   setup() {
     onLaunch(() => {
       console.log("App Launch");
+      // #ifdef H5
+      uni.getSystemInfo({
+        success(res) {
+          const isMobile: boolean = /iOS|Android/i.test(res.system);
+          if (!isMobile) {
+            const basic: string = "/static/pc.html";
+            if (res.windowWidth > 768 && !/PC/i.test(location.href)) {
+              window.location.replace(basic + window.location.hash);
+            }
+            // 监听窗口大小变化
+            window.addEventListener("resize", () => {
+              if (window.innerWidth > 768) {
+                window.location.replace(basic + window.location.hash);
+              }
+            });
+          }
+        },
+      });
+      // #endif
     });
   },
 });
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+// #ifdef H5
+body::-webkit-scrollbar,
+html::-webkit-scrollbar {
+  display: none;
+}
+// #endif
+</style>
